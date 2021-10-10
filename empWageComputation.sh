27d2cf8670836e766eefa8cb244f4d359bab1278
beeru <beerappadandaragi@gmail.com>
2021-10-10 19:50:57 +0530

    [beerappa] present absent

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,3 +1,13 @@
 #!/bin/bash -x
 echo "Welcome to Employee
 Wage Computation"
+ispresent=1;
+randomcheck=$((RANDOM%2));
+
+if [ $ispresent -eq $randomcheck ];
+then
+	echo "Employee is present" ;
+else
+	echo "Employee is absent" ;
+fi
+
