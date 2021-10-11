0c1adf2565076f2bac31273c1f09aa4e645c4824
beerappa <beerappadandaragi@gmail.com>
2021-10-11 23:37:36 +0530

    Calculating Wages

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -45,5 +45,27 @@ case $empcheck in
         emphrs=0;
 	     ;;
 esac
-salary=$(($emphrs*$emprateperhr));
+#salary=$(($emphrs*$emprateperhr));
+#isparttime=1;
+#isfulltime=2;
+totalsalary=0;
+#emprateperhr=20;
+numworkdays=20;
 
+for (( day=1; day<=$numworkdays; day++))
+do
+ empcheck=$((RANDOM%3));
+ case $empcheck in
+	 $isfulltime)
+        emphrs=8 
+	;;
+     $isparttime)
+        emphrs=4
+	;;
+*) 
+	emphrs=0
+	;;
+esac
+salary=$(($emphrs*$emprateperhr));
+totalsalary=$(($totalsalary+$salary));
+done
