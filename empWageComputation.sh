44ffd1a72a4cfb6b18ea4b4d04eed552c9136a9b
beerappa <beerappadandaragi@gmail.com>
2021-10-12 15:58:03 +0530

    dictnory keys

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -96,15 +96,8 @@ do
 
 done
 
-totalsalary=$(($totalemphrs * $EMP_RATE_PER_HR)) 
-#IS_PART_TIME=1;
-#IS_FULL_TIME=2;
+totalsalary=$(($totalemphrs * $EMP_RATE_PER_HR)); 
 MAX_HRS_IN_MONTH=4;
-#EMP_RATE_PER_HR=20;
-#NUM_WORKING_DAYS=20;
-
-#totalemphr=0;
-#totalworkingdays=0;
 function getworkhrs () {
 	local $empcheck=$1
 	case $empcheck in
@@ -126,45 +119,15 @@ do
 	((totalworkingdays++))
 	empcheck=$((RANDOM%3));
 		emphrs="$( getworkhrs $empcheck )"
-                #totalemphrs=$(($totalemphrs + $emphrs))
+                
 	    done
 
-            #totalsalary=$(($totalemphrs * $EMP_RATE_PER_HR));
-#IS_PART_TIME=1;
-#IS_FULL_TIME=2;
-#MAX_HRS_IN_MONTH=4;
-#EMP_RATE_PER_HR=20;
-#NUM_WORKING_DAYS=20;
-
-#totalemphr=0;
-#totalworkingdays=0;
-#function getworkhrs () {
-#	local $empcheck=$1
-#	case $empcheck in
-#	   $IS_FULL_TIME)
-#		emphrs=8
-#		;;
-#	   $IS_PART_TIME)
-#		emphrs=4
-#		;;
- #          *)
-#		emphrs=0
-#		;;
-#esac 
- #echo $emphrs
-#}
-#while [[ $totalemphrs -lt $MAX_HRS_IN_MONTH && $totalworkingdays -lt $NUM_WORKING_DAYS ]]
-
-#do
-#	((totalworkingdays++))
-#	empcheck=$((RANDOM%3));
-#		emphrs="$( getworkhrs $empcheck )"
-#               totalemphrs=$(($totalemphrs + $emphrs))
 		dailywage[$totalworkingdays]=$(($emphrs*$EMP_RATE_PER_HR))
 
-#	    done
+	     echo ${dailywage[@]} 
+	     
+            declare -a dailywage
 
-#             totalsalary=$(($totalemphrs * $EMP_RATE_PER_HR));
-	     echo ${dailywage[@]}
-
-
+             dailywage["$totalworkingdays"]=$(($emphrs*$EMP_RATE_PER_HR))
+	
+	     echo ${!dailywage[@]}
